53277954ba34f5aaa6b9ee15ea38764ad48904ca
zsj <2879979437@qq.com>
2025-04-09 18:04:39 +0800

    商品浏览业务功能代码（与套餐管理）

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -2,6 +2,7 @@ package com.sky.service;
 
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.mapper.DishMapper;
 import com.sky.result.PageResult;
 import com.sky.vo.DishVO;
@@ -51,4 +52,17 @@ public interface DishService {
      * @param id
      */
     void startOrStop(Integer status, Long id);
+
+    /**
+     * 根据分类id查询菜品
+     * @param categoryId
+     * @return
+     */
+    List<Dish> list(Long categoryId);
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 }
